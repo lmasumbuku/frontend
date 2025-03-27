33b70b66199d030c3aac3b2b489c1cd3f0c1762c
lmasumbuku <l.masumbuku@gmail.com>
2025-03-27 08:58:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RegisterForm from "./components/RegisterForm";
@@ -6,13 +6,21 @@ import LoginForm from "./components/LoginForm";
 import MenuList from "./components/MenuList";
 import Orders from "./components/Orders";
 import AddMenuItem from "./components/AddMenuItem";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsAuthenticated(!!token);
+  }, []);
+
   return (
     <Router>
       <div>
+        <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
+
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterForm />} />

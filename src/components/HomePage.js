c4c75732b6512c4c88e06728f2bbf92e55a875a8
lmasumbuku <l.masumbuku@gmail.com>
2025-03-27 09:37:50 +0100

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,18 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  const token = localStorage.getItem("token");
+
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Bienvenue sur la plateforme restaurateur 🍽️</h1>
-      <h2>Commencez ici :</h2>
-      <div style={{ marginTop: "20px" }}>
-        <Link to="/register" style={{ marginRight: "20px", fontSize: "18px" }}>
-          🔸 S'inscrire
-        </Link>
-        <Link to="/login" style={{ fontSize: "18px" }}>
-          🔑 Se connecter
-        </Link>
-      </div>
+    <div>
+      <h2>Bienvenue sur la plateforme restaurateur 🍽️</h2>
+
+      {!token ? (
+        <>
+          <h3>Commencez ici :</h3>
+          <p>
+            <Link to="/register">❤️ S'inscrire</Link>{" "}
+            <Link to="/login">🔑 Se connecter</Link>
+          </p>
+        </>
+      ) : (
+        <p>Vous êtes connecté. Utilisez le menu ci-dessus pour naviguer.</p>
+      )}
     </div>
   );
 };

f94a8d0d40df074aee4fca3d9f66eef8b643bedd
lmasumbuku <l.masumbuku@gmail.com>
2025-05-28 19:54:51 +0200

    Create IaOrdersPage.js

---
+++ src/components/IaOrdersPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function IaOrdersPage() {
+  return (
+    <div>
+      <h2>Commandes IA</h2>
+      <p>Cette page affichera les commandes passées par l’IA.</p>
+    </div>
+  );
+}
+
+export default IaOrdersPage;
